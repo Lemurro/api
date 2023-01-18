9cf1c092b62250273f3fa737f88f801e96f1d38e
DimNS <atomcms@ya.ru>
2023-01-18 12:47:16 +0500

    feat [SettingsGeneral] Удалена константа PRODUCTION и добавлена новая константа SERVER_TYPE

--- app/Configs/SettingsGeneral.example.php
+++ app/Configs/SettingsGeneral.example.php
@@ -8,16 +8,37 @@ namespace Lemurro\Api\App\Configs;
 class SettingsGeneral
 {
     /**
-     * Это боевой сервер если стоит значение true
-     *
-     * @var boolean
-     */
-    const PRODUCTION = false;
-
-    /**
      * Имя проекта
      *
      * @var string
      */
     const APP_NAME = 'Lemurro';
+
+    /**
+     * Вид сервера
+     *
+     * @var string
+     */
+    public const SERVER_TYPE = self::SERVER_TYPE_DEV;
+
+    /**
+     * Вид сервера: разработка
+     *
+     * @var string
+     */
+    public const SERVER_TYPE_DEV = 'dev';
+
+    /**
+     * Вид сервера: тестовый
+     *
+     * @var string
+     */
+    public const SERVER_TYPE_STAGE = 'stage';
+
+    /**
+     * Вид сервера: боевой
+     *
+     * @var string
+     */
+    public const SERVER_TYPE_PROD = 'prod';
 }
